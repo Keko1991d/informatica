5a515a9f705b402cd528269a7aa0e5d016d7c906
Keko1991d <165578696+Keko1991d@users.noreply.github.com>
2024-08-04 00:31:07 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,25 @@
 // scripts.js
 
-document.addEventListener("DOMContentLoaded", () => {
-    const links = document.querySelectorAll('nav a[href^="#"]');
+document.addEventListener('DOMContentLoaded', () => {
+    const modal = document.getElementById('image-modal');
+    const modalImage = document.getElementById('modal-image');
+    const closeModal = document.querySelector('.close');
 
-    links.forEach(link => {
-        link.addEventListener("click", e => {
-            e.preventDefault();
-            const targetId = link.getAttribute("href").substring(1);
-            const targetElement = document.getElementById(targetId);
-
-            window.scrollTo({
-                top: targetElement.offsetTop - document.querySelector('header').offsetHeight,
-                behavior: "smooth"
-            });
+    document.querySelectorAll('.view-image').forEach(button => {
+        button.addEventListener('click', (event) => {
+            const imageUrl = event.target.getAttribute('data-image');
+            modalImage.src = imageUrl;
+            modal.style.display = 'flex';
         });
+    });
+
+    closeModal.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
     });
 });
