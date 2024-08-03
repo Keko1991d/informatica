846d927fdee5c917d60be5b6122f0a3b5005a885
Keko1991d <165578696+Keko1991d@users.noreply.github.com>
2024-08-03 02:55:33 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// scripts.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll('nav a[href^="#"]');
+
+    links.forEach(link => {
+        link.addEventListener("click", e => {
+            e.preventDefault();
+            const targetId = link.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetElement.offsetTop - document.querySelector('header').offsetHeight,
+                behavior: "smooth"
+            });
+        });
+    });
+});
